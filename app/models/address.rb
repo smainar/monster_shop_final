2391c013e93a667daa7c12d50fbc6e0f31675e9b
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-27 15:27:52 -0600

    Add validations and relationships to Address model

---
+++ app/models/address.rb
@@ -0,0 +1,10 @@
+class Address < ApplicationRecord
+  belongs_to :user
+  # has_many :orders
+
+  validates_presence_of :street,
+                        :city,
+                        :state,
+                        :zip,
+                        :nickname
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Address, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :street}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+    it {should validate_presence_of :nickname}
+  end
+
+  describe "relationships" do
+    it {should belong_to :user}
+    # it {should have_many :orders}
+  end
+end
