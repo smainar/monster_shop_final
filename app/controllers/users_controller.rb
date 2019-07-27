eab546de73bc4d6704c7e7aace82471cd7fd695c
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-27 15:36:47 -0600

    Update methods (new and user_params) in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @address = @user.addresses.build
   end
 
   def create
@@ -44,6 +45,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :address, :city, :state, :zip, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, addresses_attributes: [:id, :nickname, :street, :city, :state, :zip])
   end
 end
