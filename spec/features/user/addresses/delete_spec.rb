a1950b1c0b13f2edc2949dfad6fba340f87e9a11
Stella Mainar <mainars09@alumni.hanover.edu>
2019-07-30 11:04:26 -0600

    Update feature tests for Destroy an Address by calling @user.reload and revisiting the User Profile Page

--- spec/features/user/addresses/delete_spec.rb
+++ spec/features/user/addresses/delete_spec.rb
@@ -4,28 +4,26 @@ RSpec.describe "Destroy an Address", type: :feature do
   describe "As a Registered User" do
     before(:each) do
       @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
-      
+
       @user_address_1 = @user.addresses.create!(street: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, nickname: "work")
       @user_address_2 = @user.addresses.create!(street: '987 Spruce St', city: 'Denver', state: 'CO', zip: 80210, nickname: "home")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
     it "I can delete an address from my profile page" do
-      visit login_path
-
-      fill_in "Email", with: @user.email
-      fill_in "Password", with: @user.password
-      click_button("Log In")
-
       visit profile_path
 
       within "#address-#{@user_address_1.id}" do
         expect(page).to have_link("Delete Address")
         click_link("Delete Address")
       end
-
+      expect(page).to have_content("Address was deleted!")
       expect(current_path).to eq(profile_path)
 
-      expect(page).to have_content("Address was deleted!")
+      @user.reload
+      visit profile_path
+
       expect(page).to have_css("section#address-#{@user_address_2.id}")
       expect(page).to have_content(@user_address_2.street)
       expect(page).to have_content(@user_address_2.zip)
